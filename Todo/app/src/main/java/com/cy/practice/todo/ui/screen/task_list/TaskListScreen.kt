9ce5cafd75163d7e49e1d46a7cad8f5687f83184
chiayu <chiayu.dev@gmail.com>
2025-03-31 01:14:06 +0800

    change add task to bottom sheet

--- Todo/app/src/main/java/com/cy/practice/todo/ui/screen/task_list/TaskListScreen.kt
+++ Todo/app/src/main/java/com/cy/practice/todo/ui/screen/task_list/TaskListScreen.kt
@@ -1,31 +1,28 @@
 package com.cy.practice.todo.ui.screen.task_list
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.cy.practice.todo.domain.model.Task
+import com.cy.practice.todo.ui.screen.task_list.component.NewTaskBottomSheet
 import com.cy.practice.todo.ui.screen.task_list.component.TaskList
 import com.cy.practice.todo.ui.theme.TodoTheme
 
@@ -36,13 +33,14 @@ fun TaskListScreen(modifier: Modifier = Modifier, vm: TaskListViewModel = hiltVi
     TaskListScreen(uiState, vm::onAction, modifier)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskListScreen(
     state: TaskListState,
     onAction: (TaskListAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var name by rememberSaveable { mutableStateOf("") }
+    var showAddTask by rememberSaveable { mutableStateOf(false) }
 
     Column(modifier = modifier.imePadding()) {
         TaskList(
@@ -53,41 +51,29 @@ fun TaskListScreen(
             { task, isChecked ->
                 onAction(TaskListAction.EditTask(task.copy(isDone = isChecked)))
             },
-            { task ->
-                onAction(TaskListAction.DeleteTask(task))
-            },
+            { task -> onAction(TaskListAction.DeleteTask(task)) },
             modifier = Modifier.weight(1f)
         )
-        Row {
-            TextField(
-                value = name,
-                onValueChange = { name = it },
-                placeholder = { Text("Enter new task") },
-                textStyle = LocalTextStyle.current.copy(),
-                trailingIcon = {
-                    IconButton(
-                        enabled = name.isNotBlank(),
-                        onClick = {
-                            val newTask = Task(name = name)
-                            onAction(TaskListAction.AddTask(newTask))
-                        }
-                    ) {
-                        Icon(
-                            Icons.Default.Add,
-                            contentDescription = "Add Task",
-                        )
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(100.dp))
-                    .weight(1f),
-                colors = TextFieldDefaults.colors(
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent,
-                ),
+
+        TextButton(
+            onClick = { showAddTask = true },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add new task",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
             )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Add Task")
         }
+    }
+
+    if (showAddTask) {
+        NewTaskBottomSheet(
+            { showAddTask = false },
+            { task -> onAction(TaskListAction.AddTask(task)) }
+        )
     }
 }
 
